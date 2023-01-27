99323a1a83d1315486d2b1942872a0698c346f93
Rodrigo Zegóbia <rodrigozegobia@gmail.com>
2023-01-27 16:00:15 -0300

    Update script.js
    Adds function to play one round

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ function getComputerChoice() {
     var randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }
+console.log(getComputerChoice())
+
+function playRound(playerSelection, computerSelection) {
+    var result;
+    
+    if (playerSelection === computerSelection) {
+      result = "It's a Tie!";
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors" ||
+               playerSelection === "Scissors" && computerSelection === "Paper" ||
+               playerSelection === "Paper" && computerSelection === "Rock") {
+      result = "You Win! " + playerSelection + " beats " + computerSelection;
+    } else {
+      result = "You Lose! " + computerSelection + " beats " + playerSelection;
+    }
+    return result;
+}
